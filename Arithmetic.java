fb33a3cc9805786cc4613993788ae74ef1402ce5
Ayush001122 <140638790+Ayush001122@users.noreply.github.com>
2023-07-27 21:50:04 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,26 @@
+ public class Arithmetic{
+    public static void main(String args[]){
+        int a=4;    // value 1
+        int b=2;    // value 2
+
+        // addition
+        int c=a+b;
+        System.out.println("Add: " + c);
+
+        // subtraction
+        int d=a-b;
+        System.out.println("Subtract: " + d);
+        
+        // multiplication
+        int e=a*b;
+        System.out.println("Multiplication: " + e);
+
+        // division
+        int f=a/b;
+        System.out.println("Divide:" + f);
+
+        // modulus
+        int g=a%b;
+        System.out.println("Remainder: " + g);
+    }
+}
