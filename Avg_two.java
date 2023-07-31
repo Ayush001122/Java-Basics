3df67891956aa5274f9e2ba19fac1fc51908e63c
Ayush001122 <140638790+Ayush001122@users.noreply.github.com>
2023-07-31 21:45:27 +0530

    Add files via upload

---
+++ Avg_two.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Avg_two{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int a=sc.nextInt();
+        System.out.print("Enter number 2: ");
+        int b=sc.nextInt();
+        float avg=(a+b)/2;
+        System.out.println("Average of 2 numbers: " + avg );
+    }
+}
+
+
