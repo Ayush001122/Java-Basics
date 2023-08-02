d4996109461673793dfb062b3439ddb0cf5f20cb
Ayush001122 <140638790+Ayush001122@users.noreply.github.com>
2023-08-02 21:43:09 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,100 @@
+// OPERATORS
+
+//== equal 
+// public class Operators{
+//     public static void main(String args[]){
+//         int a=10;
+//         int b=5;
+//         System.out.println(a==b);
+//     }
+// }
+
+// != not equal to
+// public class Operators{
+//     public static void main(String args[]){
+//         int a=10;
+//         int b=5;
+//         System.out.println(a!=b);
+//     }
+// }
+
+// > greater than
+// public class Operators{
+//     public static void main(String args[]){
+//         int a=10;
+//         int b=5;
+//         System.out.println(a>b);
+//     }
+// }
+
+// < less than
+// public class Operators{
+//     public static void main(String args[]){
+//         int a=15;
+//         int b=14;
+//         System.out.println(a<b);
+//     }
+// }
+
+// >= greater than or equal to
+// public class Operators{
+//     public static void main(String args[]){
+//         int a=10;
+//         int b=10;
+//         System.out.println(a>=b);
+//     }
+// }
+
+// <= less than or equal to 
+// public class Operators{
+//     public static void main(String args[]){
+//         int a=10;
+//         int b=10;
+//         System.out.println(a<=b);
+//     }
+// }
+
+// && logical and
+// public class Operators{
+//     public static void main(String args[]){
+//         System.out.println((3>2) && (5>0));
+//     }
+// }
+
+// || logical or
+// public class Operators{
+//     public static void main(String args[]){
+//         System.out.println((4>2) || (3>0));
+//     }
+// }
+
+// logical not
+// public class Operators{
+//     public static void main(String args[]){
+//         System.out.println(!(3>2));
+//     }
+// }
+
+// Assignment operator
+// public class Operators{
+//     public static void main(String args[]){
+//         int a=10;
+//         int b=5;
+//         int c=5;
+//         int d=5;
+//         int e=5;
+//         int f=20;
+//         a+=10;
+//         System.out.println(a);
+//         b-=5;
+//         System.out.println(b);
+//         c*=5;
+//         System.out.println(c);
+//         d/=5;
+//         System.out.println(d);
+//         e%=5;
+//         System.out.println(e);
+//         a=f;
+//         System.out.println(a);
+//     }
+// }
