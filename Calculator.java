dfdfcdc5de5db70799eeca8111acd2a7deb16e95
Ayush001122 <140638790+Ayush001122@users.noreply.github.com>
2023-08-07 21:26:49 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Calculator{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter value a: ");
+        float a=sc.nextInt();
+        System.out.print("Enter value b: ");
+        float b=sc.nextInt();
+        System.out.print("Enter operator here: ");
+        char operator=sc.next().charAt(0);
+        switch(operator){
+            case '+':
+            System.out.println("Answer: "+ (a+b));
+            break;
+            case '-':
+            System.out.println("Answer: "+ (a-b));
+            break;
+            case '*':
+            System.out.println("Answer: "+ (a*b));
+            break;
+            case '/':
+            System.out.println("Answer: "+ (a/b));
+            break;
+            case '%':
+            System.out.println("Answer: "+ (a%b));
+            break;
+            default:
+            System.out.println("Wrong operator");
+        }
+    }
+}
