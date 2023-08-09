7ca1e37d2134377c9697fd5e52ba1516de18eb63
Ayush001122 <140638790+Ayush001122@users.noreply.github.com>
2023-08-09 21:23:55 +0530

    Add files via upload

---
+++ Positive_negative.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Positive_negative{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your value here: ");
+        int a= sc.nextInt();
+        if (a<0){
+            System.out.println("Number is negative");
+        }
+        else if (a>0){
+            System.out.println("Number is positive");
+        }
+        else
+        System.out.println("Number is zero");
+    }
+}
+
