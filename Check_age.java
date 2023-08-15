c6137acc5648b3a61b9a98884bab34cd01e828ae
Ayush001122 <140638790+Ayush001122@users.noreply.github.com>
2023-08-15 20:57:50 +0530

    Add files via upload

---
+++ Check_age.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Check_age{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter age of Student A: ");
+        int a= sc.nextInt();
+        System.out.print("Enter age of Student B: ");
+        int b= sc.nextInt();
+        System.out.print("Enter age of Student C: ");
+        int c= sc.nextInt();
+        if (a>=b && a>=c){
+        System.out.println("A is older");
+        }
+        else if (b>=c){
+        System.out.println("B is older");
+        }
+        else
+        System.out.println("C is older");
+    }
+}
+
